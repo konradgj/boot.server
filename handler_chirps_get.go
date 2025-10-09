a06f74220884691dec27a8f0b010c7c61dd3d6fa
konradgj <konrad.gjerstad@gmail.com>
2025-10-09 23:33:10 +0200

    CH9: Documentation L1: Documentation

--- handler_chirps_get.go
+++ handler_chirps_get.go
@@ -4,13 +4,31 @@ import (
 	"net/http"
 
 	"github.com/google/uuid"
+	"github.com/konradgj/boot.server/internal/database"
 )
 
 func (cfg *apiConfig) handlerChirpsList(w http.ResponseWriter, req *http.Request) {
-	chirps, err := cfg.database.ListChirps(req.Context())
-	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Couldn't retrieve chirps", err)
-		return
+	authorId := req.URL.Query().Get("author_id")
+	var chirps []database.Chirp
+	var err error
+	if authorId != "" {
+		id, err := uuid.Parse(authorId)
+		if err != nil {
+			respondWithError(w, http.StatusBadRequest, "Couldn't parse author_id", err)
+			return
+		}
+
+		chirps, err = cfg.database.ListChirpsByAuthor(req.Context(), id)
+		if err != nil {
+			respondWithError(w, http.StatusInternalServerError, "Couldn't retrieve chirps", err)
+			return
+		}
+	} else {
+		chirps, err = cfg.database.ListChirps(req.Context())
+		if err != nil {
+			respondWithError(w, http.StatusInternalServerError, "Couldn't retrieve chirps", err)
+			return
+		}
 	}
 
 	response := make([]Chirp, len(chirps))
--- sql/queries/chirps.sql
+++ sql/queries/chirps.sql
@@ -20,6 +20,9 @@ RETURNING
 -- name: ListChirps :many
 SELECT * FROM chirps ORDER BY created_at ASC;
 
+-- name: ListChirpsByAuthor :many
+SELECT * FROM chirps WHERE user_id = $1 ORDER BY created_at ASC;
+
 -- name: GetChirp :one
 SELECT * FROM chirps WHERE id = $1;
 
